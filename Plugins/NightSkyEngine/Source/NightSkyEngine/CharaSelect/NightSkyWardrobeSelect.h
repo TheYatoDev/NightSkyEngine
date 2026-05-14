94c6832c404579a8a522f151ebe7c1b2dc6ff2cf
TheYatoDev <theyatpdev@gmail.com>
2026-05-14 16:44:03 -0400

    - completed night sky menu controller

---
+++ Plugins/NightSkyEngine/Source/NightSkyEngine/CharaSelect/NightSkyWardrobeSelect.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NightSkyEngine/UI/ExtendedCommonActivatableWidget.h"
+#include "NightSkyWardrobeSelect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIGHTSKYENGINE_API UNightSkyWardrobeSelect : public UExtendedCommonActivatableWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Plugins/NightSkyEngine/Source/NightSkyEngine/Menu/NightSkyMenuController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "NightSkyMenuController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIGHTSKYENGINE_API ANightSkyMenuController : public APlayerController
+{
+	GENERATED_BODY()
+};
